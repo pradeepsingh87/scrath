d6863db72695b2e06e9f84113ee660429867ae6a
Pradeep Singh <76230497+pradeepsingh87@users.noreply.github.com>
2023-10-04 10:59:47 -0400

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,1 +1,26 @@
-aws ds describe-users --directory-id <your-directory-id> --user-ids <user-id>
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "fsx:ListTagsForResource",
+        "fsx:DescribeFileSystems",
+        "fsx:ListFiles",
+        "fsx:DescribeBackups",
+        "fsx:CreateBackup",
+        "fsx:DeleteBackup"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:DeleteObject"
+      ],
+      "Resource": "arn:aws:s3:::your-archive-bucket/*"
+    }
+  ]
+}
